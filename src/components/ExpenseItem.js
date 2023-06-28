16b1446b1b2fad029881733191f8b5e64d7f5208
jeongsangtae <wjdtkdxo55@naver.com>
2023-06-29 04:52:16 +0900

    React Complete Training - CSS style add

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>2023년 6월 29일</div>
-      <div>
+      <div className="expense-item__description">
         <h2>자동차 보험</h2>
-        <div>294000원</div>
+        <div className="expense-item__price">294000원</div>
       </div>
     </div>
   );
