9e437033f412b3da6baff323af58d66e8929b36a
jeongsangtae <wjdtkdxo55@naver.com>
2023-06-29 04:32:10 +0900

    React Complete Training - JSX code

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>2023년 6월 29일</div>
+      <div>
+        <h2>자동차 보험</h2>
+        <div>294000원</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
