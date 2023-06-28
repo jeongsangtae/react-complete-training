7cbdbc0536ac4704249d0aa994c4717ab5471e24
jeongsangtae <wjdtkdxo55@naver.com>
2023-06-29 05:31:49 +0900

    React Complete Training - JS data print

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date();
+  const expenseTitle = "자동차 보험";
+  const expensePrice = 294000;
+
   return (
     <div className="expense-item">
-      <div>2023년 6월 29일</div>
+      <div>
+        {expenseDate.getFullYear()}년 {expenseDate.getMonth() + 1}월
+        {expenseDate.getDate()}일
+      </div>
       <div className="expense-item__description">
-        <h2>자동차 보험</h2>
-        <div className="expense-item__price">294000원</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expensePrice}원</div>
       </div>
     </div>
   );
