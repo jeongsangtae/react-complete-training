edc6d8ccca8221baa6020f19fc281697e74ba5a4
jeongsangtae <wjdtkdxo55@naver.com>
2023-06-30 05:43:48 +0900

    React Complete Training - date css add + js logic add

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.getMonth() + 1;
+  const day = props.date.getDate();
+
   return (
     <div className="expense-item">
-      <div>
-        {props.date.getFullYear()}년 {props.date.getMonth() + 1}월
-        {props.date.getDate()}일
+      <div className="expense-date">
+        <div className="expense-date__year">{year}</div>
+        <div className="expense-date__month">{month}</div>
+        <div className="expense-date__day">{day}</div>
       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
