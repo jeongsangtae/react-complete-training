d76ff2dba0230b0fc34ef5203c7ab372266c5171
jeongsangtae <wjdtkdxo55@naver.com>
2023-07-06 04:18:08 +0900

    React Complete Training - event listener & event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,6 +5,10 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -12,6 +16,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.price}원</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
