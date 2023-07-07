739fade1358c72d8fbb63b5abae6d1cc3b0773d7
jeongsangtae <wjdtkdxo55@naver.com>
2023-07-08 05:07:42 +0900

    Recat Complete Training - two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -49,6 +49,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredPrice("");
+    setEnteredDate("");
   };
 
   return (
@@ -56,7 +59,11 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Price</label>
@@ -64,6 +71,7 @@ const ExpenseForm = () => {
             type="number"
             min="100"
             step="100"
+            value={enteredPrice}
             onChange={priceChangeHandler}
           />
         </div>
@@ -73,6 +81,7 @@ const ExpenseForm = () => {
             type="date"
             min="2020-01-01"
             max="2023-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
