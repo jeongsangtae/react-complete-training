3ed83d9248853854f332387919b95930675036cc
jeongsangtae <wjdtkdxo55@naver.com>
2023-07-10 20:53:16 +0900

    React Complete Training - Expenses, ExpenseFilter comment add

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,15 +6,21 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  // ExpensesFilter.js에 "2023"이 초기 값으로 설정되는 값 지정
   const [filteredYear, setFilteredYear] = useState("2023");
 
+  // 콜백 함수
+  // ExpenseFilter.js 컴포넌트에서 전달된 filter의 값인 event.target.value를 매개변수 selectedYear로 저장하고 useState로 데이터 출력
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
+
   return (
     <div>
       <Card className="expenses">
+        {/* ExpenseFilter.js에 onChangeFilter라는 이름의 prop으로 filterChangeHandler라는 콜백 함수 전달 */}
+        {/* selsected라는 이름의 prop을 "2023"이 초기 값으로 설정되어있는 state를 ExpenseFilter.js에 전달 */}
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = (props) => {
+  // Expenses 컴포넌트의 props로 전달된 onChangeFilter 콜백 함수를 호출해 select에서 선택된 값인 event.target.value를 가져오고 그 데이터를 Expenses.js에 인자로 전달
   const filterChangeHandler = (event) => {
     props.onChangeFilter(event.target.value);
     // console.log(event.target.value);
@@ -12,6 +13,8 @@ const ExpensesFilter = (props) => {
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
+        {/* select를 통해서 선택한 값에서 filterChangeHandler 함수가 작동 */}
+        {/* Expenses.js에서 받은 state 초기 값 설정 */}
         <select value={props.selected} onChange={filterChangeHandler}>
           <option value="2023">2023</option>
           <option value="2022">2022</option>
