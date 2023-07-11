3491f13cf0580d9cbd80ad21b0fe79972f8d87ab
jeongsangtae <wjdtkdxo55@naver.com>
2023-07-12 04:27:46 +0900

    React Complete Training - map(data rendering)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -25,26 +25,13 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
-          title={props.items[0].title}
-          price={props.items[0].price}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          price={props.items[1].price}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          price={props.items[2].price}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          price={props.items[3].price}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            price={expense.price}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
