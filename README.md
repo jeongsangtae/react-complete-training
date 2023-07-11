edc20ee7f7d5ee1196ec43aff3a04a5323b3698b
jeongsangtae <wjdtkdxo55@naver.com>
2023-07-12 05:20:21 +0900

    React Complete Training - Using state stored lists(state update)

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React 실습
 
 1. React 기초 및 실습 컴포넌트
+
+2. React State 및 이벤트 다루기
+
+3. 렌더링 리스트 및 조건부 Content
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
+const DUMMY_EXPENSES = [
+  { id: "e1", title: "휴지", price: `18,000`, date: new Date(2021, 2, 21) },
+  { id: "e2", title: "TV", price: `800,000`, date: new Date(2022, 0, 30) },
+  {
+    id: "e3",
+    title: "자동차 보험",
+    price: `294,000`,
+    date: new Date(2023, 5, 30),
+  },
+  { id: "e4", title: "책상", price: `120,000`, date: new Date(2023, 4, 30) },
+];
+
 const App = () => {
-  const expenses = [
-    { id: "e1", title: "휴지", price: `18,000`, date: new Date(2021, 2, 21) },
-    { id: "e2", title: "TV", price: `800,000`, date: new Date(2022, 0, 30) },
-    {
-      id: "e3",
-      title: "자동차 보험",
-      price: `294,000`,
-      date: new Date(2023, 5, 30),
-    },
-    { id: "e4", title: "책상", price: `120,000`, date: new Date(2023, 4, 30) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   // 콜백 함수
   // NewExpense.js 컴포넌트에서 전달된 expenseData를 expense라는 매개변수로 사용해 데이터 출력
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   // 구 프로젝트 셋업
