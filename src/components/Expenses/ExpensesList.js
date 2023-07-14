326f5c98da30117df0c8d4053db383025bd3038d
jeongsangtae <wjdtkdxo55@naver.com>
2023-07-14 23:28:50 +0900

    React Complete training - ExpensesList comment add + form conditional statement add

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -4,10 +4,12 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
+  // if문의 조건을 만족했을 때 if문 내의 return에 있는 내용 출력
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback">데이터 없음</h2>;
   }
 
+  // if문의 조건을 만족하지 않았을 때 여기있는 return의 내용을 출력
   return (
     <ul className="expenses-list">
       {props.items.map((expense) => (
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   // 콜백 함수
   // ExpenseForm.js 컴포넌트에서 전달된 expenseData를 enteredExpenseData라는 매개변수로 사용
   // enteredExpenseData라는 매개변수를 받는데 이 매개변수는 ExpenseForm.js 컴포넌트에서 사용자가 입력한 데이터 (ExpenseForm.js의 expenseData 객체)
@@ -15,12 +16,29 @@ const NewExpense = (props) => {
     };
     // NewExpense 컴포넌트의 props로 전달된 onAddExpense 콜백 함수를 호출하고, 입력된 내용에 id를 추가한 expenseData를 App.js에 인자로 전달
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      {/* ExpenseForm.js에 onSaveExpenseData라는 이름의 prop으로 saveExpenseDataHandler라는 콜백 함수 전달 */}
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        //ExpenseForm.js에 onSaveExpenseData라는 이름의 prop으로 saveExpenseDataHandler라는 콜백 함수 전달
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
